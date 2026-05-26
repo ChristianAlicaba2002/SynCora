88522ace56c1f9b74f43cc61a07aba2f8fc04845
ChristianAlicaba2002 <christiandave120702@gmail.com>
2026-05-26 12:30:53 +0800

    refactor: Simplify login store implementation by consolidating state and setters into initial state object

--- app/store/loginStore.ts
+++ app/store/loginStore.ts
@@ -1,95 +1,41 @@
 import { create } from "zustand";
 
-interface ILoginStore {
-    email: string;
-    setEmail: (value: string) => void;
-
-    password: string;
-    setPassword: (value: string) => void;
-
-    showPassword: boolean;
-    setShowPassword: (value: boolean) => void;
-
-    isError: boolean;
-    setIsError: (value: boolean) => void;
-
-    emailErrorMessage: string;
-    setEmailErrorMessage: (value: string) => void;
-
-    passwordErrorMessage: string;
-    setPasswordErrorMessage: (value: string) => void;
-
-    isLoading: boolean;
-    setIsLoading: (value: boolean) => void;
+interface LoginStore {
+  email: string;
+  password: string;
+  showPassword: boolean;
+  isError: boolean;
+  emailErrorMessage: string;
+  passwordErrorMessage: string;
+  isLoading: boolean;
+  setEmail: (value: string) => void;
+  setPassword: (value: string) => void;
+  setShowPassword: (value: boolean) => void;
+  setIsError: (value: boolean) => void;
+  setEmailErrorMessage: (value: string) => void;
+  setPasswordErrorMessage: (value: string) => void;
+  setIsLoading: (value: boolean) => void;
+  reset: () => void;
 }
 
-const LoginStore = create<ILoginStore>((set) => ({
-    email: "",
-    setEmail: (value) => set(() => ({
-        email: value
-    })),
+const initialState = {
+  email: "",
+  password: "",
+  showPassword: false,
+  isError: false,
+  emailErrorMessage: "",
+  passwordErrorMessage: "",
+  isLoading: false,
+};
 
-    password: "",
-    setPassword: (value) => set(() => ({
-        password: value
-    })),
-
-    showPassword: false,
-    setShowPassword: (value) => set((prev) => ({
-        showPassword: value
-    })),
-
-    isError: false,
-    setIsError: (value) => set(() => ({
-        isError: value
-    })),
-
-    emailErrorMessage: "",
-    setEmailErrorMessage: (value) => set(() => ({
-        emailErrorMessage: value
-    })),
-
-    passwordErrorMessage: "",
-    setPasswordErrorMessage: (value) => set(() => ({
-        passwordErrorMessage: value
-    })),
-
-    isLoading: false,
-    setIsLoading: (value: boolean) => set(() => ({
-        isLoading: value
-    }))
-}))
-
-export const useLoginStore = () => {
-    const email = LoginStore((state) => state.email);
-    const setEmail = LoginStore((state) => state.setEmail);
-    const password = LoginStore((state) => state.password);
-    const setPassword = LoginStore((state) => state.setPassword);
-    const showPassword = LoginStore((state) => state.showPassword);
-    const setShowPassword = LoginStore((state) => state.setShowPassword);
-    const isError = LoginStore((state) => state.isError);
-    const setIsError = LoginStore((state) => state.setIsError);
-    const emailErrorMessage = LoginStore((state) => state.emailErrorMessage);
-    const setEmailErrorMessage = LoginStore((state) => state.setEmailErrorMessage);
-    const passwordErrorMessage = LoginStore((state) => state.passwordErrorMessage);
-    const setPasswordErrorMessage = LoginStore((state) => state.setPasswordErrorMessage);
-    const isLoading = LoginStore((state) => state.isLoading);
-    const setIsLoading = LoginStore((state) => state.setIsLoading);
-
-    return {
-        email,
-        setEmail,
-        password,
-        setPassword,
-        showPassword,
-        setShowPassword,
-        isError,
-        setIsError,
-        emailErrorMessage,
-        setEmailErrorMessage,
-        passwordErrorMessage,
-        setPasswordErrorMessage,
-        isLoading,
-        setIsLoading,
-    }
-}
+export const useLoginStore = create<LoginStore>((set) => ({
+  ...initialState,
+  setEmail: (value) => set({ email: value }),
+  setPassword: (value) => set({ password: value }),
+  setShowPassword: (value) => set({ showPassword: value }),
+  setIsError: (value) => set({ isError: value }),
+  setEmailErrorMessage: (value) => set({ emailErrorMessage: value }),
+  setPasswordErrorMessage: (value) => set({ passwordErrorMessage: value }),
+  setIsLoading: (value) => set({ isLoading: value }),
+  reset: () => set(initialState),
+}));
