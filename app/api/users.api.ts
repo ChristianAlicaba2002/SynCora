66b199b5c8889e7df9075c8fe43e0e3db83d0a6e
ChristianAlicaba2002 <christiandave120702@gmail.com>
2026-05-28 22:14:25 +0800

    feat: Implement LoginUserAPI for user authentication, enhancing login functionality

--- app/api/users.api.ts
+++ app/api/users.api.ts
@@ -1,8 +1,14 @@
-import type { TRegisterUser } from "../@types";
+import type { TLoginUser, TRegisterUser } from "../@types";
 import { api } from "./axios";
 
+
+export const LoginUserAPI = async (data: TLoginUser) => {
+    const response = await api.post("/users/login", data);
+    return response.data
+}
 
 export const RegisterUserAPI = async (data: TRegisterUser) => {
     const response = await api.post("/users/register", data);
     return response.data
 }
+
