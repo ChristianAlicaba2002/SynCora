4177fd8ac02f9b71f3cacac6037a17acc72a105f
ChristianAlicaba2002 <christiandave120702@gmail.com>
2026-05-28 19:02:05 +0800

    feat: Wrap RootLayout in QueryClientProvider to enable React Query for data management

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,16 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Stack } from "expo-router";
 
+const queryClient = new QueryClient();
+
 export default function RootLayout() {
-  return <Stack>
-    <Stack.Screen name="index" options={{ headerShown: false }} />
-    <Stack.Screen name="register" options={{ headerShown: false }} />
-    <Stack.Screen name="home" options={{ headerShown: false }} />
-  </Stack>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Stack>
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="register" options={{ headerShown: false }} />
+        <Stack.Screen name="home" options={{ headerShown: false }} />
+      </Stack>
+    </QueryClientProvider>
+  );
 }
