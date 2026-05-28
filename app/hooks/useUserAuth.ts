0869217220e940ba560d90c01b5c0c683b1c6840
ChristianAlicaba2002 <christiandave120702@gmail.com>
2026-05-28 22:14:34 +0800

    feat: Add useLoginUser hook for user authentication, integrating LoginUserAPI with React Query

--- app/hooks/useUserAuth.ts
+++ app/hooks/useUserAuth.ts
@@ -1,5 +1,12 @@
 import { useMutation } from "@tanstack/react-query";
-import { RegisterUserAPI } from "../api/users.api";
+import { LoginUserAPI, RegisterUserAPI } from "../api/users.api";
+
+export const useLoginUser = () => {
+    return useMutation({
+        mutationKey: ["login"],
+        mutationFn: LoginUserAPI
+    })
+}
 
 export const useRegisterUser = () => {
     return useMutation({
