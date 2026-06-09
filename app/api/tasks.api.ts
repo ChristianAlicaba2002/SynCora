7476b35829df208abdbaf79fa70bb91f8253671d
ChristianAlicaba2002 <christiandave120702@gmail.com>
2026-06-09 11:02:26 +0800

    feat(api): Add allTasksAPI to fetch all tasks from the server
    - Implement allTasksAPI function to retrieve a list of all tasks
    - Enhance task management capabilities by providing a dedicated API endpoint

--- app/api/tasks.api.ts
+++ app/api/tasks.api.ts
@@ -1,6 +1,11 @@
 import type { TCreateTask, TTask, TTasksResponse, TUpdateTask } from "../@types";
 import { api } from "./axios";
 
+export const allTasksAPI = async (): Promise<TTask[]> => {
+  const response = await api.get<TTasksResponse>("/tasks/all");
+  return response.data.data;
+};
+
 export const createTaskAPI = async (data: TCreateTask) => {
   const response = await api.post("/tasks", data);
   return response.data;
